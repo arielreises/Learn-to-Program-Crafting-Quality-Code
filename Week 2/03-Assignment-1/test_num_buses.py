66d497f06272484b17c8a38a018f64cc28ef3bf8
Ariel Reises <arielreises2@gmail.com>
2023-11-06 21:46:01 -0300

    Create test_num_buses.py

---
+++ Week 2/03-Assignment-1/test_num_buses.py
@@ -0,0 +1,20 @@
+import a1
+import unittest
+
+class TestNumBuses(unittest.TestCase):
+    # Test class for function a1.num_buses. """
+
+    def test_min_buses_exact(self):
+        # Test with an exact multiple of bus capacity. """
+        self.assertEqual(a1.num_buses(100), 2)
+
+    def test_min_buses_uneven(self):
+        # Test with a number that requires extra bus capacity. """
+        self.assertEqual(a1.num_buses(103), 3)
+
+    def test_min_buses_zero(self):
+        # Test with zero people. """
+        self.assertEqual(a1.num_buses(0), 0)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
