ff7faac2ba7107a00b9c2d4fd46390ee5de81a59
Ariel Reises <arielreises2@gmail.com>
2023-11-11 07:56:50 -0300

    Update test_stock_price_summary.py

--- Week 2/03-Assignment-1/test_stock_price_summary.py
+++ Week 2/03-Assignment-1/test_stock_price_summary.py
@@ -3,6 +3,8 @@
 # Student: Ariel Ladislau Reises
 # Github: github.com/arielreises
 
+# Revision 1: carried out after feedback from Robert Minkler
+
 import a1
 import unittest
 
@@ -20,6 +22,28 @@ class TestStockPriceSummary(unittest.TestCase):
     def test_price_changes_all_gains(self):
         # Test with all positive price changes.
         self.assertEqual(a1.stock_price_summary([0.02, 0.05, 0.1]), (0.17, 0))
+
+    # New tests, suggested by Robert Minkler
+    
+    def test_price_changes_empty(self):
+        # Test with an empty list of price changes.
+        self.assertEqual(a1.stock_price_summary([]), (0, 0))
+
+    def test_price_changes_one_element(self):
+        # Test with a list containing only one element.
+        self.assertEqual(a1.stock_price_summary([0.05]), (0.05, 0))
+
+    def test_price_changes_all_losses(self):
+        # Test with a list containing only negative values.
+        self.assertEqual(a1.stock_price_summary([-0.02, -0.1, -0.05]), (0, -0.17))
+
+    def test_price_changes_large_gains(self):
+        # Test with a list containing large positive values.
+        self.assertEqual(a1.stock_price_summary([100, 200, 300]), (600, 0))
+
+    def test_price_changes_large_losses(self):
+        # Test with a list containing large negative values.
+        self.assertEqual(a1.stock_price_summary([-100, -200, -300]), (0, -600))
 
 if __name__ == '__main__':
     unittest.main(exit=False)
